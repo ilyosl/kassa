822817cf0befed637355f75961194f4e5f405b8f
ilyosl <ilyosl@mail.ru>
2023-01-07 16:07:13 +0500

    initial new project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center mt-16">
+       <h1 className="text-4xl text-center">
+         Tailwind CSS makes styling React components easier!
+       </h1>
+       <button className="bg-black text-white p-2.5 w-fit mt-9 ">
+         Get Started
+       </button>
+     </div>
+  );
+}
+
+export default App;
